39050f0910ff8310332fc144064e64ee75364412
vzhovtan <ccie5732@gmail.com>
2024-12-05 14:34:32 -0800

    adding test function

---
+++ README.md
@@ -0,0 +1,1 @@
+** Simple webapp to shorten urls
--- internal/url/url_test.go
+++ internal/url/url_test.go
@@ -1,1 +1,34 @@
 package url
+
+// TBD - refactor to use interfaces
+// TBD - use interfaces for DB mockup
+import (
+	"testing"
+)
+
+func TestShortenURL(t *testing.T) {
+	tests := []struct {
+		name     string
+		original string
+		want     string
+	}{
+		{
+			name:     "given the original valid url, getting back the shorten one, 1st case",
+			original: "www.google.com",
+			want:     "191347bf",
+		},
+		{
+			name:     "given the original valid url, getting back the shorten one, 2nd case",
+			original: "www.yahoo.com",
+			want:     "d61a0fed",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ShortenURL(tt.original)
+			if got != tt.want {
+				t.Errorf("ShortenURL  = %v, wanted %v", got, tt.want)
+			}
+		})
+	}
+}
